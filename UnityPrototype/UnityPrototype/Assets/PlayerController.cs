a1e06dbd23d598eb805802984b4d256092ac7d20
George Croucher <32357071+gpcroucher@users.noreply.github.com>
2019-02-13 12:02:22 +0000

    first commit of Unity prototype

---
+++ UnityPrototype/UnityPrototype/Assets/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera firstPersonCamera;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    private Vector3 hookLocation;
+    private bool hookPulling = false;
+
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // Movement speed in units/sec.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TurnCamera();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootHook();
+        }
+
+        if (hookPulling)
+        {
+            PullToHook();
+        }
+    }
+
+    
+
+    private void TurnCamera()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+    private void ShootHook()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(firstPersonCamera.transform.position, firstPersonCamera.transform.TransformDirection(Vector3.forward), out hit))
+        {
+            hookLocation = hit.point;
+            hookPulling = true;
+
+            startMarker = transform;
+
+            // Keep a note of the time the movement started.
+            startTime = Time.time;
+
+            // Calculate the journey length.
+            journeyLength = Vector3.Distance(startMarker.position, hookLocation);
+
+            print("ray hit");
+        }
+    }
+
+    private void PullToHook()
+    {
+        // Distance moved = time * speed.
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed = current distance divided by total distance.
+        float fracJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(startMarker.position, hookLocation, fracJourney);
+    }
+}
