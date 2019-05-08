ec0493864b24683d6352375c125436b1d7fb485b
S195905 <46591703+S195905@users.noreply.github.com>
2019-05-08 19:08:11 +0100

    Working Main Menu and Level Select

---
+++ Prototypes/Unity/UnityPrototypeV2/UnityPrototype/UnityPrototype/Assets/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+}   
+
